b887e33e9ef8309e6e84bbf9523d640a51fb4f1e
Liezl Puzon <puzon@stanford.edu>
2017-02-16 16:06:08 -0800

    Add firebase init code back in

--- app.js
+++ app.js
@@ -4,10 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 
 var app = express();
+
+// FIREBASE
+var firebase = require('firebase-admin');
+var API_KEY = "AAAA4aMrHeI:APA91bFoDhKYhDaTJfUo-dVd1UY9oVOWZ-dhQSWGJcq6V11-0Ud59uHDxsAeMFpY7oxP6FGC9Qb_N7tPUADeBog_La2RCkP25tz31UPJSoNdQnUDQ1gTL0Ql92nGvX2YBJhzvv_nHOMp_O5sL3aIkyLkdaE1IhYqxQ";
+
+const serviceAccount = require('./serviceAccountKey.json')
+const client1 = "eHIJfESLuqg:APA91bH6946NRzPhFClGLcXYDkhwCItACSSdhY-Fffm1EGqgy51pbbk4vnjoomDcps3HPqpNJLNZ5CFIHj0jtzcgtfz_Lvot2TdsIZwTi_ig0b7a5jS7efooeBC3wuBoV1jcsoc0C7BI";
+
+// INTIALIZE FIREBASE
+// TODO: figure out how to make this a global reference thru all express routes!
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://kirontestapp.firebaseio.com/"
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	// TODO: get emails from Firebase server
 	results = {}
   res.render('index', { title: 'Search', names: results});
 });
