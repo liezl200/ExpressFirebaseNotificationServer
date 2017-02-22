40f7cb7ac9ae361196d2941e5d351564f385ee47
Liezl Puzon <puzon@stanford.edu>
2017-02-22 14:46:03 -0800

    Add route to send notification

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	// TODO: get emails from Firebase server
+	// TODO: get topics from Firebase server
 	var usersRef = firebase.database().ref().child('users');
 	var users = [];
 	usersRef.orderByChild('email') // try to look up this user in our firebase db users table
@@ -14,11 +14,41 @@ router.get('/', function(req, res, next) {
     		var email = userData.val().email;
     		users.push({'email': email, 'name': email.substring(0, email.indexOf('@')), 'fcmTokens': userData.val().fcmTokens});
     	});
-    	console.log(users);
+    	// console.log(users);
     	// console.log(snapshot.val());
+        res.render('index', { title: 'Search', sendResults: null, users: users});
     });
-	results = {}
-  res.render('index', { title: 'Search', names: results, users: users});
+});
+
+/* POST to send notification */
+router.post('/send', function(req, res, next) {
+    var targetEmails = req.body.emails;
+    var topics = req.body.tags;
+
+    // console.log('send');
+    // console.log(targetEmails);
+    // console.log(topics);
+
+    if (emails) {
+        // get FCM tokens from Firebase server -- only if emails are specified
+        var usersRef = firebase.database().ref().child('users');
+        var users = [];
+        usersRef.orderByChild('email') // try to look up this user in our firebase db users table
+        .on('value', function(snapshot) {
+            snapshot.forEach(function(userData) {
+                console.log(userData.val());
+                var email = userData.val().email;
+                users.push({'email': email, 'name': email.substring(0, email.indexOf('@')), 'fcmTokens': userData.val().fcmTokens});
+            });
+            // console.log(users);
+            // console.log(snapshot.val());
+
+            // TODO: send notification to the listed recipients based on the retrieved FCM tokens
+        });
+    } else { // send to all users
+
+    }
+
 });
 
 module.exports = router;
