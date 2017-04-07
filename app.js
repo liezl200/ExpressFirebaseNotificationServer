568efcfb2786a0ef51ae7cf6a7df8a571df213c7
Liezl Puzon <puzon@stanford.edu>
2017-04-07 15:00:07 +0200

    Clean up app.js

--- app.js
+++ app.js
@@ -13,22 +13,17 @@ firebase = require('firebase-admin');
 var API_KEY = "AAAA4aMrHeI:APA91bFoDhKYhDaTJfUo-dVd1UY9oVOWZ-dhQSWGJcq6V11-0Ud59uHDxsAeMFpY7oxP6FGC9Qb_N7tPUADeBog_La2RCkP25tz31UPJSoNdQnUDQ1gTL0Ql92nGvX2YBJhzvv_nHOMp_O5sL3aIkyLkdaE1IhYqxQ";
 
 const serviceAccount = require('./serviceAccountKey.json')
-// const client1 = "eHIJfESLuqg:APA91bH6946NRzPhFClGLcXYDkhwCItACSSdhY-Fffm1EGqgy51pbbk4vnjoomDcps3HPqpNJLNZ5CFIHj0jtzcgtfz_Lvot2TdsIZwTi_ig0b7a5jS7efooeBC3wuBoV1jcsoc0C7BI";
 
 // INTIALIZE FIREBASE
-// TODO: figure out how to make this a global reference thru all express routes!
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
   databaseURL: "https://kirontestapp.firebaseio.com/"
 });
 
-//app.locals.firebase = firebase;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -67,6 +62,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
